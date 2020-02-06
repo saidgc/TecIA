cd1ba36d47c3af2114fd83ad1cf3b8b86354ad36
Said <said.g@hotmail.com>
2020-02-06 13:08:02 -0600

    ya quedo la version 3

--- tarea1.py
+++ tarea1.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 
 actual_temp = 1
 previous_temp = 2
@@ -12,13 +13,13 @@ coeff = 0
 
 
 def temperature(previous_temp, gas_percentage, fan):
-    return (previous_temp + (5 * gas_percentage) - (10 * fan) - 1)
+    return (previous_temp + (20 * gas_percentage) - (10 * fan) - 1)
 
 
 if __name__ == "__main__":
     while True:
         actual_temp = temperature(previous_temp, gas_percentage, fan)
-        print("gas = {0:2.0f} fan = {1:1.2f} temp = {2:3.0f}".format(
+        print("gas = {0:2.2f} fan = {1:2.2f} temp = {2:3.0f}".format(
             gas_percentage, fan, actual_temp))
         previous_temp = actual_temp
         if learning:
@@ -29,15 +30,18 @@ if __name__ == "__main__":
             while n >= 0: 
                 point_b += 1
                 n -= difference
-            learning = False
             coeff = (0 - point_a) / (point_b - 2)
+            reduce_factor = temperature(1000,0,10) - temperature(1000,0,9)
+            learning = False
         else:
-            if actual_temp < limit + 2:
-                gas_percentage = round((actual_temp - point_a) / coeff + 1, 0)
+            if actual_temp < limit + 1:
+                gas_percentage = (actual_temp - point_a) / coeff + 1
                 if gas_percentage > 100:
                     gas_percentage = 100
                 fan = 0
             else:
                 gas_percentage = 0
-                fan = .2
+                fan = (limit - actual_temp) / reduce_factor
+                if fan > 10:
+                    fan = 10
         time.sleep(1)
